7fc413b799ae61cb29a2c8b62039ee62b1488254
Mo Saleh <mohamed.is8890@gmail.com>
2019-12-10 18:55:16 +0200

    Introduced a form and fired the effect programatically

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "./styles.css";
 
 function App() {
- const [data, setData] = useState({ hits: [] });
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
+  const [search, setSearch] = useState("redux");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("https://hn.algolia.com/api/v1/search?query=" + search)
+      .then(result => {
+        setData(result.data);
+        setIsLoading(false);
+      });
+  }, [search]);
+
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          setSearch(query);
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
