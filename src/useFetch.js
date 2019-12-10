2beb3e28456eb9b7600644ab067be01610fa9cb4
Mo Saleh <mohamed.is8890@gmail.com>
2019-12-10 19:50:32 +0200

    With useReducer hook

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,18 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const fetchReducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING_START":
+      return { ...state, isLoading: true };
+
+    case "LOADING_END":
+      return { ...state, isLoading: false };
+
+    case "FETCH_COMPLETE":
+      return { ...state, data: action.payload };
+
+    default:
+      return { ...state };
+  }
+};
+
 export default function useFetch(initialUrl, initialData) {
-  const [data, setData] = useState(initialData);
   const [search, setSearch] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
+
+  const [state, dispatch] = useReducer(fetchReducer, {
+    data: initialData,
+    search: initialUrl,
+    isLoading: false
+  });
 
   useEffect(() => {
-    setIsLoading(true);
+    dispatch({ type: "LOADING_START" });
+
     axios.get(search).then(result => {
-      setData(result.data);
-      setIsLoading(false);
+      dispatch({ type: "FETCH_COMPLETE", payload: result.data });
+      dispatch({ type: "LOADING_END" });
     });
   }, [search]);
 
-  return [{ data, isLoading }, setSearch];
+  return [state, setSearch];
 }
